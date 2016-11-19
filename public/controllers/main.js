ce292c46b166aab157aeb82be4dc923af9840669
Hunter DeGroot <hunterdegroot@gmail.com>
2016-11-19 17:18:46 -0600

    production issue still

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -5,7 +5,7 @@ mainApp.controller("MainCtrl", function ($http) {
     var url = "http://104.236.118.111:3000";
 
     app.saveProduct = function (newProduct) {
-        $http.post(url + "/add", {name:newProduct}).success(function () {
+        $http.post(url + "/add", {name: newProduct}).success(function () {
         });
     };
 });
@@ -14,18 +14,17 @@ mainApp.controller("EngineCtrl", function ($http) {
 
     var url = "http://104.236.118.111:3000";
 
-    var lock = false;
     basicScene = new BasicScene();
 
     function loadPlayers() {
         console.log(basicScene.user.kill);
-        if(basicScene.user.kill == true) {
-            $http.get(url+"/kill").success(function (r) {
+        if (basicScene.user.kill == true) {
+            $http.get(url + "/kill").success(function (r) {
                 console.log(r);
             });
             basicScene.user.kill = false;
             var len = ids.length;
-            for (var i = 4; i < 4+len; i++) {
+            for (var i = 4; i < 4 + len; i++) {
                 var obj = basicScene.scene.children[i];
                 basicScene.scene.remove(obj);
             }
@@ -42,21 +41,19 @@ mainApp.controller("EngineCtrl", function ($http) {
         });
     };
 
-    function saveUser (playerId,x,y,rot) {
-        $http.post(url + "/update", {playerId:playerId, x:x, y:y, rot: rot}).success(function () {
+    function saveUser(playerId, x, y, rot) {
+        $http.post(url + "/update", {playerId: playerId, x: x, y: y, rot: rot}).success(function () {
             loadPlayers();
         });
-        lock = false;
     };
 
     function animate() {
-        if(lock == false) {
-            saveUser(basicScene.user.id,basicScene.user.mesh.position.x,basicScene.user.mesh.position.z, basicScene.user.mesh.rotation.y);
-            requestAnimationFrame(animate);
-            setTimeout( loadOtherPlayers(), 0, 200 );
-             loadOtherPlayers();
-            basicScene.frame();
-        }
+        basicScene.frame();
+        saveUser(basicScene.user.id, basicScene.user.mesh.position.x, basicScene.user.mesh.position.z, basicScene.user.mesh.rotation.y);
+        requestAnimationFrame(animate);
+        setTimeout(loadOtherPlayers(), 0, 200);
+        loadOtherPlayers();
     }
+
     animate();
 });
