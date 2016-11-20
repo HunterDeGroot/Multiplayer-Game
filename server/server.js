fd6600864da5366fd40de61edfa5e8e32bff0b0e
Hunter DeGroot <hunterdegroot@gmail.com>
2016-11-19 19:12:35 -0600

    production issue still

--- server/server.js
+++ server/server.js
@@ -20,9 +20,8 @@ mongoose.connect('mongodb://104.236.118.111/db');
 var Player = mongoose.model('Player', {playerId: Number,x: Number,y: Number,rot: Number});
 
 app.get("/kill", function (req, res) {
-    Player.remove({}, function(err, result) {
-        return res.send("succesfully killed zombs");
-    });
+    Player.remove({});
+    res.send("succesfully killed zombs");
 });
 
 app.get("/game", function (req, res) {
